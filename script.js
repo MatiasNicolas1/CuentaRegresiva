1b58dc3c24167fb29ab83ed886a5085e52126c5b
Matias Nicolas <81336235+MatiasNicolas1@users.noreply.github.com>
2024-07-04 16:52:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// Fecha objetivo: 27 de julio
+const targetDate = new Date("July 27, 2024 00:00:00").getTime();
+
+const countdown = () => {
+  const now = new Date().getTime();
+  const timeLeft = targetDate - now;
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+  if (timeLeft < 0) {
+    clearInterval(interval);
+    document.getElementById("countdown").innerHTML =
+      "<h1>¡La fecha ha llegado!</h1>";
+  }
+};
+
+const interval = setInterval(countdown, 1000);
+countdown();
